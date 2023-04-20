7f5e4b04b579b5efafc44672c4e593cc368bd611
tejsinghani-mohit <58900107+tejsinghani-mohit@users.noreply.github.com>
2023-04-20 19:41:09 +0530

    Add files via upload

---
+++ pr.sh
@@ -0,0 +1,19 @@
+echo "Opening a Pull Request"
+
+curl -X 'POST' \
+  'http://139.59.21.103:3000/api/v1/repos/siddharth/gitops-argocd/pulls' \
+  -H 'accept: application/json' \
+  -H "authorization: $ARGOCD_TOKEN" \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "assignee": "siddharth",
+  "assignees": [
+    "siddharth"
+  ],
+  "base": "main",
+  "body": "Updated deployment specification with a new image version.",
+  "head": "feature-gitea",
+  "title": "Updated Solar System Image"
+}'
+
+echo "Success"
